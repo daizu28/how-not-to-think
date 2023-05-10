3b5688077c284bd08c93160d01e3907b938dcb45
daizu28 <kinakodaizukinako@gmail.com>
2023-05-11 06:36:38 +0900

    firstcommit

---
+++ jtalk_test.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+import subprocess
+from datetime import datetime
+
+def jtalk(t):
+    open_jtalk=['open_jtalk']
+    mech=['-x','/usr/local/Cellar/open-jtalk/1.11/dic/install-mecab-ipadic-neologd']
+    htsvoice=['-m','/Users/life_mac_09/Desktop/test_voice/models/openjtalk_mei/mei_normal.htsvoice']
+    speed=['-r','1.0']
+    outwav=['-ow','open_jtalk.wav']
+    cmd=open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+    c.stdin.write(t.encode())
+    c.stdin.close()
+    c.wait()
+    aplay = ['afplay','-q','open_jtalk.wav']
+    wr = subprocess.Popen(aplay)
+
+def say_datetime():
+    d = datetime.now()
+    text = 'newspaper' 
+    jtalk(text)
+
+if __name__ == '__main__':
+    say_datetime()
+
